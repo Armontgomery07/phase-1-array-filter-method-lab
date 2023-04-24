53cc3296f48f3041b043117c397086cc73493d99
Arielle Montgomery <Armontgomery07@gmail.com>
2023-04-23 20:37:54 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, hometown) {
+    return drivers.filter(driver =>
+      driver.toLowerCase() === hometown.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(drivers, hometown) {
+    return drivers.filter(driver =>
+      driver.toLowerCase().startsWith(hometown.toLowerCase())
+    );
+  }
+  
+  function matchName(drivers, hometown) {
+    return drivers.filter(driver =>
+      driver.name.toLowerCase() === hometown.toLowerCase()
+    );
+  }
